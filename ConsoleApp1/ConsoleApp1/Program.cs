caad9cb63c4ffcdab98c9e871fda09c32aa1fabd
Jacob <jacobmk@gmail.com>
2024-09-01 18:20:02 +0530

    testdev10 changes

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,8 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello3");
-            Console.WriteLine("Hello3");
+            Console.WriteLine("Hello30");
+            Console.WriteLine("Hello30");
         }
     }
 }
